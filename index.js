862c8a7c9d85b6d1760c8f80b6aac36f323de0a5
mHammy <133612424+mHammy@users.noreply.github.com>
2023-07-30 17:41:55 -0400

    Had my first successful run with the license requirements per acceptance criteria

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+function getLicenseBadgeURL(license) {
+    return `https://img.shields.io/badge/license-${encodeURIComponent(license)}-brightgreen`;
+  }
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -24,9 +28,16 @@ const questions = [
         message: 'Enter usage instructions:',
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'Enter the license for your project:',
+        message: 'Choose a license for your project:',
+        choices: [
+            'MIT License',
+            'Apache License 2.0',
+            'GNU GPLv3',
+            'ISC License',
+            'None',
+        ],
     },
     {
         type: 'input',
@@ -45,6 +56,14 @@ const questions = [
     },
 ];
 
+const licenseExplanations = {
+    'MIT License': 'A permissive license that allows users to do almost anything with the code as long as they provide attribution back to the author and don’t hold the author liable. This license is widely used for its simplicity and permissiveness.',
+    'Apache License 2.0': 'A permissive license that allows users to use, modify, and distribute the licensed software. The Apache License 2.0 also provides explicit protection against patent infringement claims.',
+    'GNU GPLv3': 'The latest version of the GPL, which addresses some of the compatibility issues of GPLv2 and provides more protection against software patents.',
+    'ISC License': 'The ISC License simply removes language that would be considered extraneous according to the Berne Convention. The ISC License is considered equivalent to the Simplified (2-Clause) BSD License but with more concise language.',
+    'None': 'No license has been chosen for this project',
+};
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFileSync(fileName, data);
@@ -55,8 +74,15 @@ function init() {
     inquirer
         .prompt(questions)
         .then((answers) => {
+            const licenseBadgeURL = getLicenseBadgeURL(answers.license);
+            const licenseExplanation = licenseExplanations[answers.license];
             const readmeContent = `
 # ${answers.title}
+
+## License
+![License](${licenseBadgeURL})
+
+${licenseExplanation}
 
 ## Description
 ${answers.description}
@@ -74,9 +100,6 @@ ${answers.installation}
 
 ## Usage
 ${answers.usage}
-
-## License
-${answers.license}
 
 ## Contributing
 ${answers.contributing}
