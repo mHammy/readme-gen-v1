92193744ffdea6dbc9b8f96bff4225030d4a0568
mHammy <133612424+mHammy@users.noreply.github.com>
2023-07-30 17:23:03 -0400

    Updated the index.js with basic functionality. Currently it is not at the acceptance criteria level, however it is functional

--- index.js
+++ index.js
@@ -1,13 +1,99 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage instructions:',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Enter the license for your project:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter frequently asked questions and their answers:',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const readmeContent = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+${answers.questions}
+`;
+            writeToFile('README.md', readmeContent);
+            console.log('README.md created successfully');
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
 
 // Function call to initialize app
 init();
